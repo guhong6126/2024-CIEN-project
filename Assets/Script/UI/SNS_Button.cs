157543a24df30c3b9f6aaba92b9be4f0e19b9cd6
guhong <redcircle12@naver.com>
2024-07-13 20:57:16 +0900

    UI

---
+++ Assets/Script/UI/SNS_Button.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SNS_Button : MonoBehaviour
+{
+    public void OnLButton()
+    { 
+        
+    }
+
+    public void OnRButton()
+    {
+
+    }
+}
