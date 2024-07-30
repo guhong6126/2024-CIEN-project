1c98dd890605e9e921ac98770abf4cdb95644335
jin-the-newbie <eunjinchung225@gmail.com>
2024-07-31 01:38:25 +0900

    Random Select
    랜덤으로 선택하는 알고리즘만 구현해뒀습니다.

---
+++ Assets/Script/Data/MessageGenerator.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using System.Linq;
+
+public enum Authenticity
+{
+    true1, false1, breaking
+}
+
+public enum FalseElements
+{
+    location, methods, picture
+}
+
+public class MessageGenerator : MonoBehaviour
+{
+    public Authenticity real_authenticity;
+    public FalseElements false_elt;
+    private PersistentData persistentData = PersistentData.Instance; //PersistentData 인스턴스에 접근하기
+
+    
+
+
+
+    private List<string> messages = new List<string>
+    {
+        "{0}에서 {1} 하는 테러 집단 봄..ㄷㄷ 뭐임? 모래시계 같은데",
+        "나 지금 {0}인데 {1} 하는 장면 목격한 것 같음... 개무섭다;",
+        "혹시 이거 {1} 하는 거 맞나요? 지금 {0}인데 걱정 되네요..."
+    };
+    private List<string> news = new List<string>
+    {
+        "[속보] 모래시계, {0}규모의 테러 조직으로 판명",
+        "[속보] 모래시계는 {0}규모 테러 조직"
+    };
+    private List<string> nicknames = new List<string>
+    {
+        "고니","안졸리나젤리","MrBeast","뿌뿌뽕"
+    };
+    private List<string> presses = new List<string>
+    {
+        "BBC","CNN","The New York Times"
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //나중에 클래스 만들고 나서 위치 전부 바꿔야 함
+        persistentData = PersistentData.Instance;
+        if (persistentData == null)
+        {
+            Debug.LogError("PersistentData instance is null. Please check the initialization.");
+            return;
+        }
+
+        Terror_scale scale = persistentData.current_scale;
+        Terror_location location = persistentData.current_location;
+        List<Terror_methods> methods = persistentData.current_methods;
+
+        Authenticity[] authenticities = (Authenticity[])System.Enum.GetValues(typeof(Authenticity)); // create a list containing terror locations (테러 위치 리스트 생성)
+        real_authenticity = authenticities[Random.Range(0, authenticities.Length)];
+
+
+        SetRandomMessage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+    void SetRandomMessage()
+    {
+        string printed_message = string.Empty; //출력할 메시지(아직은 empty)
+
+        string selected_message = messages[Random.Range(0, messages.Count)]; // 게시물 멘트 랜덤 선택
+
+        // 가짜로 적을 항목 랜덤 선택
+        if (real_authenticity == Authenticity.false1)
+        {
+            FalseElements[] false_elts = (FalseElements[])System.Enum.GetValues(typeof(FalseElements));
+            false_elt = false_elts[Random.Range(0, false_elts.Length)];
+        }
+        
+
+        // 닉네임도 뽑아야 함...
+        string nickname = nicknames[Random.Range(0, nicknames.Count)]; // 닉네임 랜덤 선택
+
+
+        // 문구 대충 틀만..
+
+        if (real_authenticity == Authenticity.true1 || (real_authenticity == Authenticity.false1 && false_elt== FalseElements.picture)) // 진위 여부가 참이거나 [진위여부는 거짓인데 그림이 거짓]인 경우
+        {
+            Terror_methods current_method = persistentData.current_methods[Random.Range(0, persistentData.current_methods.Count)]; // 게시물에 담을 방법(참) 랜덤 선택
+            printed_message = string.Format(selected_message, persistentData.current_location, current_method); // 게시물 문구(참) 만들기
+        }
+        else {
+            if(false_elt == FalseElements.location)
+            {
+                Terror_methods current_method = persistentData.current_methods[Random.Range(0, persistentData.current_methods.Count)]; // 게시물에 담을 방법(참) 랜덤 선택
+
+                // 거짓지역 랜덤 선택
+                List<Terror_location> allLocations = ((Terror_location[])System.Enum.GetValues(typeof(Terror_location))).ToList(); // 일단 모든 지역 가져오기
+                allLocations.Remove(persistentData.current_location); // 거기서 현재 지역 지우기
+                Terror_location false_location = allLocations[Random.Range(0, allLocations.Count)]; // 지역 하나 랜덤 선택
+
+
+                printed_message = string.Format(selected_message, false_location, current_method); // 게시물 문구(거짓 지역) 만들기
+            }
+
+            else if(false_elt == FalseElements.methods)
+            {
+                // 거짓방법 랜덤 선택        
+                Terror_methods[] allMethods = (Terror_methods[])System.Enum.GetValues(typeof(Terror_methods)); // 모든 method 가져오기
+                List<Terror_methods> excludedMethods = new List<Terror_methods>();
+                foreach (Terror_methods method in allMethods)
+                {
+                    if (!persistentData.current_methods.Contains(method))
+                    {
+                        excludedMethods.Add(method);
+                    }
+                }
+                Terror_methods false_method = excludedMethods[Random.Range(0, excludedMethods.Count)];
+
+                printed_message = string.Format(selected_message, persistentData.current_location, false_method); // 게시물 문구(거짓 방법) 만들기
+
+            }
+        }
+        
+
+        Debug.Log(printed_message);
+    }
+    void SetRandomPicture()
+    {
+        
+        // if (게시물==거짓 && 거짓항목==그림)
+        if (real_authenticity == Authenticity.false1 && false_elt == FalseElements.picture) {
+            //출력그림=(모래시계=0이거나 거짓 방법에 해당하는 그림 선택)
+        }
+        else{
+            //출력그림=(모래시계=1이고 참인 방법에 해당하는 그림 선택)
+        }
+        
+    }
+
+    void SetBreakingNews()
+    {
+        // 속보인지 체크는 얘를 호출하는 함수에서 해둘 예정... if(authenticity==속보){출력메시지=SetBreakingNews();} else{출력메시지=SetRandomMessage();} 이렇게
+        // 그래서 여기서는 내용만 구성해주면 될듯
+
+        // 언론사 랜덤 선택 구현하기
+
+
+        // 문구 대충 틀만..
+        /**
+        string selected_message = news[Random.Range(0, news.Count)]; // 게시물 멘트 랜덤 선택
+        public string t_scale;
+        switch (current_scale)
+        {
+            case Terror_scale.small:
+                t_scale = "소";
+                break;
+            case Terror_scale.medium:
+                t_scale = "중";
+                break;
+            case Terror_scale.large:
+                t_scale = "대";
+                break;
+        }
+        string printed_message = string.Format(selected_message, t_scale); // 게시물 문구(참) 만들기
+        **/
+
+        // 그림은 SetRandomPicture 쓰면 됨
+
+    }
+}
---
+++ Assets/Script/Data/PersistentData.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/** 처음에 인코딩을 다르게 해서 한글 주석이 깨져가지고 영어로 주석을 적어뒀는데
+ * 게시물에 넣을 문구를 적으면서 생각해보니 고치긴 해야 할 문제라... 해결하면서 영어 주석을 다시 한국어로 바꿔뒀습니다
+ * 시야 문제로 놓친 번역이 있을 수도 있습니다. 혹시 이해되지 않는 부분이 있다면 언제든 말씀해주세요~
+ * */
+
+public enum Terror_scale
+{
+    small, medium, large
+}
+public enum Terror_location
+{
+    east, west, south, north, city
+}
+public enum Terror_methods
+{
+    // 인질극, 납치, 폭탄, 사이버, 바이오, 핵, 사보타주
+    hostage, kidnapping, bomb, cyber, bioterror, nuclear, sabotage
+    // 대규모일 경우 게시물에 거짓으로 나타낼 방법을 추가해야 함 (테스트를 위해 임시로 sabotage 추가해둠)
+}
+
+public class PersistentData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static PersistentData Instance;
+    public Terror_scale current_scale;
+    public List<Terror_methods> current_methods;
+    public Terror_location current_location;
+
+    
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            SetCurrentScale();
+            SetCurrentTLocation(); // 게임 메인 화면에서 클릭 입력 받았을 때 실행되게 바꿔야 함 (지금은 테스트하려고 Awake에 넣어둠)
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    /// <summary>
+    /// function to randomly set terror scale (테러 규모를 랜덤으로 설정하는 함수)
+    /// </summary>
+    public void SetCurrentScale()
+    {
+        Terror_scale[] terror_scales = (Terror_scale[])System.Enum.GetValues(typeof(Terror_scale)); // create a list containing terror scales (테러 규모를 담는 리스트 생성)
+        current_scale = terror_scales[Random.Range(0, terror_scales.Length)]; // select a random element from the list and set it as the current terror scale (리스트에서 규모 하나 랜덤 선택)
+        SetTerrorMethods();
+    }
+    /// <summary>
+    /// function to randomly set terror location (테러 위치를 랜덤으로 설정하는 함수)
+    /// </summary>
+    public void SetCurrentTLocation()
+    {
+        Terror_location[] terror_locations = (Terror_location[])System.Enum.GetValues(typeof(Terror_location)); // create a list containing terror locations (테러 위치 리스트 생성)
+        current_location = terror_locations[Random.Range(0, terror_locations.Length)]; // select a random element from the list and set it as the current terror location (리스트에서 랜덤 선택)
+    }
+    /// <summary>
+    /// function to update list of terror methods (테러 방법 리스트를 업데이트하는 함수)
+    /// </summary>
+    void SetTerrorMethods()
+    {
+        current_methods = new List<Terror_methods>(); // 실행 가능한 테러 방법을 담을 리스트
+
+        switch (current_scale) // 지금 규모가 뭐냐
+        {
+            case Terror_scale.small:
+                current_methods.Add(Terror_methods.hostage);
+                current_methods.Add(Terror_methods.kidnapping);
+                break;
+            case Terror_scale.medium:
+                current_methods.Add(Terror_methods.hostage);
+                current_methods.Add(Terror_methods.kidnapping);
+                current_methods.Add(Terror_methods.bomb);
+                current_methods.Add(Terror_methods.cyber);
+                break;
+            case Terror_scale.large:
+                current_methods.Add(Terror_methods.hostage);
+                current_methods.Add(Terror_methods.kidnapping);
+                current_methods.Add(Terror_methods.bomb);
+                current_methods.Add(Terror_methods.cyber);
+                current_methods.Add(Terror_methods.bioterror);
+                current_methods.Add(Terror_methods.nuclear);
+                break;
+        }
+
+        /** can use if-statement instead of switch-statement like below (아래처럼 if문으로 작성도 가능)
+            idk which one is more readable... (뭐가 더 가독성이 좋은지 모르겠음)
+        **/
+
+        //if(current_scale == terror_scale.large)
+        //{
+        //    current_methods.Add(terror_methods.bioterror);
+        //    current_methods.Add(terror_methods.nuclear);
+        //}
+        //if (current_scale == terror_scale.large|| current_scale == terror_scale.medium)
+        //{
+        //    current_methods.Add(terror_methods.bomb);
+        //    current_methods.Add(terror_methods.cyber);
+        //}
+        //if (current_scale == terror_scale.large || current_scale == terror_scale.medium || current_scale == terror_scale.small)
+        //{
+        //    current_methods.Add(terror_methods.hostage);
+        //    current_methods.Add(terror_methods.kidnapping);
+        //}
+
+
+
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
