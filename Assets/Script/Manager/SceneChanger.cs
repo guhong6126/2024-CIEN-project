aec90c35e003262dfd5aa625e5cf090756736739
guhong <redcircle12@naver.com>
2024-08-29 20:31:46 +0900

    build

--- Assets/Script/Manager/SceneChanger.cs
+++ Assets/Script/Manager/SceneChanger.cs
@@ -6,14 +6,18 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public static int SceneCounter { get; private set; } = 1;
+
     public void ChangeSNS()
     {
         if (SceneCounter >= 7)
+        {
             SceneManager.LoadScene("Result");
-        
-        SceneManager.LoadScene($"SNS {SceneCounter}");
-        SceneCounter++;
-        
+        }
+        else
+        {
+            SceneManager.LoadScene($"SNS {SceneCounter}");
+            SceneCounter++;
+        }
     }
 
     public void ChangeSubmit()
@@ -26,10 +30,10 @@ public class ChangeScene : MonoBehaviour
         PersistentData.Instance = null;
         SceneManager.LoadScene("Main");
     }
-    
+
     public void goSNS1()
     {
-        SceneCounter = 1;
+        SceneCounter = 2; // SNS 1 이후로는 SNS 2 씬으로 넘어가게 설정
         SuccessCounter.counter = 0;
         Managers.ClearStringList();
         SceneManager.LoadScene("SNS 1");
